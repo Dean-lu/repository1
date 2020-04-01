603dd96d4ec7c6b14c3662d2224e089c6f3c8631
web-vivi <305867562@.qq.com>
2020-04-01 23:26:39 +0800

    '个人中心样式修改'

--- mobile-ui/src/modules/mine/Mine.vue
+++ mobile-ui/src/modules/mine/Mine.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="mine">
     <!-- header-->
-    <div class="header">
-      <div><van-image></van-image></div>
-      <div>
-        <van-image></van-image>
-        <van-image></van-image>
-        <span>{{user.userName}}</span>
-        <div>立即推广赚钱</div>
+    <div class="header">      
+      <div class="header_pesonal"> 
+        <div class="hearder_head">
+          <van-icon name="user-o" />
+        </div>     
+        <div class="float-left header_txt">  
+          <span>{{user.truename}} （{{user.court}}）</span>
+          <div><span class="bg-white" @click="jump('Spread')">立即推广赚钱</span></div>
+        </div>
       </div>
     </div>
     <van-row>
       <van-grid :column-num="3">
         <van-grid-item>
-          <div>1000.00</div>
+          <div>{{user.balance}}</div>
           <div>余额</div>
         </van-grid-item>
         <van-grid-item>
-          <div>1000.00</div>
-          <div>房租</div>
+          <div>{{user.rent}}</div>
+          <div>累计租金</div>
         </van-grid-item>
         <van-grid-item>
-          <div>1000.00</div>
-          <div>佣金</div>
+          <div>{{user.commission}}</div>
+          <div>累计佣金</div>
         </van-grid-item>
       </van-grid>
     </van-row>
@@ -33,8 +35,8 @@
     <van-row class="mine-list">
       <ul>
         <li @click="jump('myInfo')">
-          <div class="float-left">
-            <van-icon name="wap-home" />
+          <div class="float-left">            
+            <van-icon name="manager" />
             <span>完善个人信息</span>
           </div>
           <div class="float-right">
@@ -88,7 +90,7 @@
         </li>
         <li>
           <div class="float-left" @click="jump('Spread')">
-            <van-icon name="volume" />
+            <van-icon name="gold-coin" />
             <span>推广赚钱</span>
           </div>
           <div class="float-right">
@@ -97,7 +99,7 @@
         </li>
         <li>
           <div class="float-left" @click="jump('updateloginPass')">
-            <van-icon name="volume" />
+            <van-icon name="lock" />
             <span>修改登录密码</span>
           </div>
           <div class="float-right">
@@ -106,7 +108,7 @@
         </li>
         <li>
           <div class="float-left" @click="jump('updatepayPass')">
-            <van-icon name="volume" />
+            <van-icon name="send-gift" />
             <span>修改支付密码</span>
           </div>
           <div class="float-right">
@@ -127,7 +129,7 @@
     data () {
       return {
         user: {
-          userName:'氪24K金VIP用户'
+          
         },
       }
     },
@@ -140,7 +142,28 @@
         if(!this.$store.state.global.loginStatus){
           this.$router.push({path : '/login'})
         }
+        let that = this;
+          let param = {
+              api_token: this.$store.state.global.api_token             
+          };
+          this.$http.get(this.$store.state.global.baseUrl + 'user/edit_user',{  params: {
+            api_token: this.$store.state.global.api_token
+            }}).then(res => {
+                if(res.status == 200) {
+                  if(res.data.code == 200){
+                    that.user = res.data.data;
+                  //that.$store.state.locale.editHouseInfo = res.data.data;
+                  }else{
+                    that.$toast(res.data.msg);
+                  }
+              }else{
+                  that.$toast('获取数据失败，请重试！');
+                  
+                  return;
+              }
+            });           
       },
+
       jump(type){
         this.$router.push({path:'/'+type})
       }
@@ -148,10 +171,45 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
   .mine .header{
     min-height: 4rem;
+    background:#f7b343;
+    color:#fff;
+    
+  } 
+  .header_pesonal{
+    position:absolute;
+    top:1rem;
+    left:1.2rem;
+    font-size:0.4rem;
+    line-height: 0.8rem;
   }
+  .hearder_head{
+    float:left;
+    width:1.6rem;
+    height:1.6rem;
+    background:#fff;
+    color:#898989;
+    border-radius: 50%;
+    margin-right:0.4rem;
+    font-size:1rem;
+    line-height:1.6rem;
+    .van-icon{
+      margin-top:9px;
+    }
+    
+  }
+  .header_txt{
+    text-align: left;
+  }
+  .bg-white{
+      background:#fff;
+      color:#f7b343;
+      font-size:0.35rem;
+      padding:0.08rem; 
+      border-radius: ;
+    }
   .mine .van-image{
     width: auto;
     margin: 0.125rem;
